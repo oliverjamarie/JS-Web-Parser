bcc6cb154cdd9b316f881d8e6970c508b316a5e5
Oliver <oliverjamarie@gmail.com>
2020-10-21 13:52:05 +0200

    Update Parse, Add Extract

--- Python/Parse.py
+++ Python/Parse.py
@@ -1,72 +1,47 @@
-import urllib
-from urllib.parse import urlparse
-import urllib3
-import requests
-import json
-from bs4 import BeautifulSoup
+
+from Extract import * 
 
 #parses a Youtube search query
 
-list_json = []
-scripts = []
 
 def main ():
+    global filter_flag, parameters, url
+
     filter_flag = '&sp=EgIQAg%253D%253D' #add this to end of the url to only get channels in the query
     parameters = {'search_query': 'frenchie mcgee'+filter_flag}
+    url = 'https://www.youtube.com/results?'
 
-    webpage = requests.get('https://www.youtube.com/results?', params=parameters)
 
-    print (webpage.url)
+    json = ExtractJSON(url,parameters)
+    json.save_to_file()
 
-    print (webpage.status_code)
 
-    bs = BeautifulSoup(webpage.content, features='html.parser')
+class Parse:
+    def __init__(self, filter_flag='', url='', parameters={}):
+        self.filter_flag = filter_flag
+        self.parameters = parameters
+        self.url = url
 
-    scripts = __get_from_tag(bs,'script')
-    list_json = __get_json_list(scripts)
+#Parses a YouTube search
+class ParseYT(Parse):
+    def __init__(self, filter_flag='', parameters={}):
+        super().__init__(filter_flag, 'https://www.youtube.com/results?', parameters)
+        self.json_response = {}
+        self.scripts = []
+
+class ParseYT_Channel(ParseYT):
+    def __init__(self, channel_name='frenchie mcgee', filter_flag = '&sp=EgIQAg%253D%253D'):
+        super().__init__(
+            filter_flag, 
+            parameters = {'search_query': channel_name + filter_flag})
+
+        self.json = ExtractJSON(self.url, self.parameters)
+
+        self.json_response = self.json.json_response
+        self.scripts = self.json.scripts[:]
+
+
+
+
 
     
-def __get_from_tag(bs:BeautifulSoup,tag:str):
-    list_scripts = []
-    for i in bs.find_all(tag):
-        msg = str(i)
-        msg = msg[8:-9] #remove the start and end tags
-        list_scripts.append(msg)
-    return list_scripts
-
-def __partition_script(script:str):
-    partitioned = []
-    string = script[:]
-
-    while string.count('{') > 0:
-        part = string.partition('{')
-        msg = part[1]
-        string = part[2]
-        part = string.partition('}')
-        msg += part[0] + part[1]
-        string = part[2]
-        partitioned.append(msg)
-  
-    return partitioned
-
-def __get_json_list (scripts:list):
-    json_list = []
-    partitioned = []
-    
-    for script in scripts:
-        arr = __partition_script(script)
-        for part in arr:
-            partitioned.append(part)
-    
-    
-    for part in partitioned:
-        try:
-            obj = json.loads(part)
-            json_list.append(obj)
-        except:
-            pass
-        
-    return json_list
-
-if __name__ == "__main__":
-    main()
