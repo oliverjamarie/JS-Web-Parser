c3c87a78ed0ef5c01b3d8e37a2eb1e99bc282324
Oliver <oliverjamarie@gmail.com>
2020-10-28 11:48:08 +0100

    Fix import warnings
    Affected files 
    Python
    -- Parse.py
    -- ParseYT.py

--- Python/Parse.py
+++ Python/Parse.py
@@ -1,5 +1,6 @@
 
-from Extract import * 
+from ExtractJSON import ExtractJSON
+from ExtractScript import ExtractScript 
 
 #parses a Youtube search query
 
@@ -8,7 +9,8 @@ class Parse:
     def __init__(self, url='', parameters={}):
         self.parameters = parameters
         self.url = url
-
+        self.json = ExtractJSON(self.url, self.parameters)
+        self.scripts = ExtractScript(self.url, self.parameters)
 
 
 
--- Python/ParseYT.py
+++ Python/ParseYT.py
@@ -7,17 +7,21 @@ from ExtractJSON import ExtractJSON
 class ParseYT(Parse):
     def __init__(self, parameters={}):
         super().__init__('https://www.youtube.com/results?', parameters)
-        self.json = ExtractJSON(self.url, self.parameters)
-        self.scripts = ExtractScript(self.url, self.parameters)
+
+        self.responseContext = self.json.json_response['responseContext']
+        self.estimatedResults = self.json.json_response['estimatedResults']
+        self.contents = self.json.json_response['contents']
+        self.trackingParams = self.json.json_response['trackingParams']
+        self.topbar = self.json.json_response['topbar']
+        self.adSafetyReason = self.json.json_response['adSafetyReason']
+        
 
 class ParseYT_Channel(ParseYT):
     def __init__(self, channel_name='frenchie mcgee', filter_flag = '&sp=EgIQAg%253D%253D'):
         super().__init__( parameters = {'search_query': channel_name + filter_flag} )
 
-        """ self.json = ExtractJSON__OLD(self.url, self.parameters)
         
-        self.json_response = self.json.json_response
-        self.scripts = self.json.scripts[:] """
+        
 
         
 
